a765bfc3c991244e06c7584ede4fee683df091cb
Saif <saif.shabir12@gmail.com>
2024-02-29 21:40:12 +0500

    feat/ created a custom Typography component that should be used for all our text fields

---
+++ src/components/Custom/Typography.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react';
+import Typography, { TypographyProps } from '@mui/material/Typography';
+
+interface CustomTypographyProps extends TypographyProps {
+  children: ReactNode;
+}
+const Text: React.FC<CustomTypographyProps> = ({ children }) => {
+  return (
+    <Typography
+      style={{
+        fontFamily: 'Epilogue',
+        color: '#01001F',
+      }}
+    >
+      {children}
+    </Typography>
+  );
+};
+export default Text;
